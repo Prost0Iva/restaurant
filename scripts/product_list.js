68ab81430dadd854b7a389cf3b2f059726ecb32c
Prost0Iva <ivaprostoiva@gmail.com>
2026-07-07 10:36:16 +0300

    AutoFilling sandwiches

--- scripts/product_list.js
+++ scripts/product_list.js
@@ -1,36 +1,76 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Страница загружена');
     pageFill();
 });
 
-function pageFill() {
+async function pageFill() {
     const prod_list = document.getElementById('product_list')
-    prod_list.insertAdjacentHTML("afterbegin",
-        `<div class = "product">
-                <img class="product_market" src="assets/img/markets/subway_logo.png" alt="">
-                <div class = "product_img">
-                    <img src="assets/img/sandwiches/ovoshnoy.png" alt="">
-                </div>
-                <table class = "product_description">
-                    <thead>
-                        <th>Овощной</th>
-                    </thead>
-                    <tbody>
-                        <td>Соус и овощи на выбор</td>
-                    </tbody>
-                    <tfoot>
-                        <tr><td>Цена: 110 руб.</td></tr>
-                    </tfoot>
-                </table>
-                <div class = "product_value">
-                    <p>КОЛИЧЕСТВО</p>
-                    <div class="product_val_changer">
-                        <button class = "product_val_remove">-</button>
-                        <div class="product_val_indicator">0</div>
-                        <button class = "product_val_add">+</button>
+
+    const response = await fetch('assets/data.json');
+    const products = await response.json();
+
+    products.menu.forEach(prod => {
+        if (prod.category == "sandwiches") {
+            let market_src = products.markets[prod.market].image
+
+            prod_list.insertAdjacentHTML("afterbegin",
+            `<div class = "product">
+                    <img class="product_market" src="assets${market_src}" alt="">
+                    <div class = "product_img">
+                        <img src="assets${prod.image}" alt="">
                     </div>
-                </div>
-                <button class = "product_add_to_cart">В КОРЗИНУ</button>
-            </div>`
-    )
+                    <table class = "product_description">
+                        <thead>
+                            <th>${prod.name}</th>
+                        </thead>
+                        <tbody>
+                            <td>${prod.description}</td>
+                        </tbody>
+                        <tfoot>
+                            <tr><td>Цена: ${prod.price} руб.</td></tr>
+                        </tfoot>
+                    </table>
+                    <div class = "product_value">
+                        <p>КОЛИЧЕСТВО</p>
+                        <div class="product_val_changer">
+                            <button class = "product_val_remove">-</button>
+                            <div class="product_val_indicator">0</div>
+                            <button class = "product_val_add">+</button>
+                        </div>
+                    </div>
+                    <button class = "product_add_to_cart">В КОРЗИНУ</button>
+                </div>`
+            )
+        }
+    });
+
+    //prod_list.insertAdjacentHTML("afterbegin",
+    //    `<div class = "product">
+    //            <img class="product_market" src="assets/img/markets/subway_logo.png" alt="">
+    //            <div class = "product_img">
+    //                <img src="assets/img/sandwiches/ovoshnoy.png" alt="">
+    //            </div>
+    //            <table class = "product_description">
+    //                <thead>
+    //                    <th>Овощной</th>
+    //                </thead>
+    //                <tbody>
+    //                    <td>Соус и овощи на выбор</td>
+    //                </tbody>
+    //                <tfoot>
+    //                    <tr><td>Цена: 110 руб.</td></tr>
+    //                </tfoot>
+    //            </table>
+    //            <div class = "product_value">
+    //                <p>КОЛИЧЕСТВО</p>
+    //                <div class="product_val_changer">
+    //                    <button class = "product_val_remove">-</button>
+    //                    <div class="product_val_indicator">0</div>
+    //                    <button class = "product_val_add">+</button>
+    //                </div>
+    //            </div>
+    //            <button class = "product_add_to_cart">В КОРЗИНУ</button>
+    //        </div>`
+    //)
+    
+    document.dispatchEvent(new CustomEvent('productListFilled'));
 }
--- scripts/products.js
+++ scripts/products.js
@@ -1,39 +1,42 @@
-const products = document.querySelectorAll('.product');
 
-products.forEach(product => {
-    const add_val = product.querySelector('.product_val_add');
-    const val = product.querySelector('.product_val_indicator');
-    const remove_val = product.querySelector('.product_val_remove');
+document.addEventListener('productListFilled', function() {
 
-    const add_to_cart = product.querySelector('.product_add_to_cart');  
-    const prod_desc = product.querySelector('.product_description');  
-    const cart = document.getElementById('cart_content')
+    const products = document.querySelectorAll('.product');
+    products.forEach(product => {
+        const add_val = product.querySelector('.product_val_add');
+        const val = product.querySelector('.product_val_indicator');
+        const remove_val = product.querySelector('.product_val_remove');
 
-    add_val.addEventListener('click', function() {
-        let temp_val = Number(val.textContent)
-        temp_val += 1
-        val.textContent = temp_val
-    })
-    remove_val.addEventListener('click', function() {
-        let temp_val = Number(val.textContent)
-        if (temp_val > 0) {
-            temp_val -= 1
-        }
-        val.textContent = temp_val
-    })
-    add_to_cart.addEventListener('click', function() {
-        let name = prod_desc.children[0].firstElementChild.textContent.trim()
-        let value = Number(val.textContent)
-        let price = Number(prod_desc.children[2].firstElementChild.textContent.split(" ")[1])
-        price *= value
-        if (value > 0) {
-            let order = document.createElement('tr');
-            order.innerHTML = `<td>${name}</td> <td>${value}</td> <td>${price}</td>`
-            cart.children[1].append(order)
+        const add_to_cart = product.querySelector('.product_add_to_cart');  
+        const prod_desc = product.querySelector('.product_description');  
+        const cart = document.getElementById('cart_content')
 
-            cart.children[2].children[0].innerHTML = `<td colspan="3">Итого: ${totalPrice(cart, price)} руб.</td>`
-            //console.log(totalPrice(cart, price))
-        }
+        add_val.addEventListener('click', function() {
+            let temp_val = Number(val.textContent)
+            temp_val += 1
+            val.textContent = temp_val
+        })
+        remove_val.addEventListener('click', function() {
+            let temp_val = Number(val.textContent)
+            if (temp_val > 0) {
+                temp_val -= 1
+            }
+            val.textContent = temp_val
+        })
+        add_to_cart.addEventListener('click', function() {
+            let name = prod_desc.children[0].firstElementChild.textContent.trim()
+            let value = Number(val.textContent)
+            let price = Number(prod_desc.children[2].firstElementChild.textContent.split(" ")[1])
+            price *= value
+            if (value > 0) {
+                let order = document.createElement('tr');
+                order.innerHTML = `<td>${name}</td> <td>${value}</td> <td>${price}</td>`
+                cart.children[1].append(order)
+
+                cart.children[2].children[0].innerHTML = `<td colspan="3">Итого: ${totalPrice(cart, price)} руб.</td>`
+                //console.log(totalPrice(cart, price))
+            }
+        })
     })
 })
 
